7d35b659c8cd6c559f31a72c917ad59d04ea564e
mkawasaki <mkawasaki@sbihg.co.jp>
2018-01-20 13:51:25 +0900

    update

--- README.md
+++ README.md
@@ -6,8 +6,18 @@ create-react-app electron-react-redux-sample
 cd electron-react-redux-sample
 yarn add redux react-redux
 yarn add redux-devtools
+yarn add create-react-class
 ```
 
 ```
 yarn build
 ```
+
+```
+yarn add electron
+```
+
+```
+electron .
+```
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import explorerApp from './reducers'
+/* 状態遷移(reducers)をstoreとして取得 */
+const store = createStore(explorerApp)
+/* アプリケーションをstoreを指定して描画 */
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
